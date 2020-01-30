f8e01c9bb5c708acf9d73708a83bca381dd71f44
John Patrick M. Madrigal <jpmadrigal07@gmail.com>
2020-01-30 23:13:37 +0800

    Initial commit

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,56 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import {
+  Navbar,
+  Nav,
+  Button,
+  Container,
+  Image,
+} from "react-bootstrap"
+import React, { useState, useEffect } from "react"
+import throttle from "lodash.throttle"
+import LogoLight from '../images/markme-logo-light.png'
+import LogoDark from '../images/markme-logo-dark.png'
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+const Header = () => {
+  const [isOnTop, setIsOnTop] = useState(true)
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
+  const handleScroll = throttle(() => {
+    const currentScrollPosition = window.pageYOffset
 
-Header.defaultProps = {
-  siteTitle: ``,
+    if (currentScrollPosition === 0) {
+      setIsOnTop(true)
+    } else {
+      setIsOnTop(false)
+    }
+  }, 300)
+
+  useEffect(() => {
+    document.addEventListener("scroll", handleScroll)
+
+    return () => {
+      document.removeEventListener("scroll", handleScroll)
+    }
+  })
+
+  return (
+    <Navbar className={isOnTop ? "" : "navbar-custom"} expand="lg" sticky="top">
+      <Container>
+        <Navbar.Brand href="#home">
+          <Image src={isOnTop ? LogoLight : LogoDark} fluid />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link href="#home" className={isOnTop ? "text-light" : ""}>Home</Nav.Link>
+            <Nav.Link href="#link" className={isOnTop ? "text-light" : ""}>Why Us</Nav.Link>
+            <Nav.Link href="#link" className={isOnTop ? "text-light" : ""}>Our Services</Nav.Link>
+            <Nav.Link href="#link" className={isOnTop ? "text-light" : ""}>About</Nav.Link>
+            <Nav.Link href="#link" className={isOnTop ? "text-light" : ""}>How to</Nav.Link>
+            <Button className="ml-2 wiggle-button" variant="primary">Schedule a call</Button>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
 }
 
 export default Header
